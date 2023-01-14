c4f10c593519cf0e08d202f3229d1855149d858c
jzmujdzin <jzmujdzin@gmail.com>
2023-01-14 12:13:59 +0100

    trending testing

---
+++ auto_shorts/tiktok_trending_testing.py
@@ -0,0 +1,7 @@
+from tiktokapipy.api import TikTokAPI
+
+if __name__ == "__main__":
+    with TikTokAPI() as api:
+        challenge = api.challenge('sidemen', video_limit=20)
+        for vid in challenge.videos._light_models:
+            print(f'{vid.id} {vid.create_time}')
