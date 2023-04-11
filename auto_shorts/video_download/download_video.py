753c9302121ae310296927a1090ce7c7554f9101
Jakub Wujec <kuba1302@gmail.com>
2023-04-11 23:01:18 +0200

    Add documentation

--- auto_shorts/video_download/download_video.py
+++ auto_shorts/video_download/download_video.py
@@ -38,8 +38,38 @@ class DownloaderBase(ABC):
 
 
 class YoutubeVideoDownloader(DownloaderBase):
+    """
+    A class used to download YouTube videos and save them to a specified location.
+
+    Methods
+    -------
+    download(video_data: VideoData, save_path: Path = base_data_path,
+             bucket: str = "auto-shorts", to_s3: bool = False, save_local: bool = True) -> None:
+        Download the specified video and save it to a specified location.
+
+    download_moments(video_data: VideoData) -> VideoDataWithMoments:
+        Download the most watched moments of the video.
+
+    _download_to_mp4(save_path: Path, vide_data_full: VideoDataWithMoments, filename: str) -> None:
+        Download the video in mp4 format and save it to a specified location.
+
+    """
+
     @staticmethod
     def download_moments(video_data: VideoData) -> VideoDataWithMoments:
+        """
+        Download the most watched moments of the video.
+
+        Parameters
+        ----------
+        video_data : VideoData
+            A `VideoData` object containing the video's metadata.
+
+        Returns
+        -------
+        VideoDataWithMoments
+            A `VideoDataWithMoments` object containing the video's metadata and the most watched moments.
+        """
         moment_downloader = MostWatchedMomentsDownloader(video_id=video_data.id)
         most_watched_moments = moment_downloader.get_most_watched_moments().to_dict(
             orient="records"
@@ -54,6 +84,24 @@ class YoutubeVideoDownloader(DownloaderBase):
             vide_data_full: VideoDataWithMoments,
             filename: str,
     ) -> None:
+        """
+        Download the video in mp4 format and save it to a specified location.
+
+        Parameters
+        ----------
+        save_path : Path
+            The path where the video will be saved.
+
+        video_data_full : VideoDataWithMoments
+            A `VideoDataWithMoments` object containing the video's metadata and the most watched moments.
+
+        filename : str
+            The name of the file in which the video will be saved.
+
+        Returns
+        -------
+        None
+        """
         (
             YouTube(f"https://www.youtube.com/watch?v={vide_data_full.id}")
             .streams.filter(file_extension="mp4")
@@ -69,7 +117,28 @@ class YoutubeVideoDownloader(DownloaderBase):
             to_s3: bool = False,
             save_local: bool = True,
     ) -> None:
+        """
+        Download video data and save it to the specified directory.
+        If `to_s3` flag is set to True, the downloaded files will also be uploaded to S3 bucket.
+        If `save_local` flag is set to False, the local files will be deleted.
 
+        Parameters:
+        -----------
+        video_data : VideoData
+            VideoData object containing the necessary information to download the video
+        save_path : str
+            The directory where the downloaded files should be saved (default: base_data_path)
+        bucket : str
+            The name of the S3 bucket to which the downloaded files should be uploaded (default: "auto-shorts")
+        to_s3 : bool
+            Flag indicating whether the downloaded files should be uploaded to S3 (default: False)
+        save_local : bool
+            Flag indicating whether the downloaded files should be saved locally (default: True)
+
+        Returns:
+        --------
+        None
+        """
         if not to_s3 and not save_local:
             raise ValueError(
                 "Wrong params config! One of 'to_s3' and 'save_local' must be True!"
