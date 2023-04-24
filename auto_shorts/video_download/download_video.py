f5c650061fe65bea13130be812a7ffa95399a058
Jakub Wujec <kuba1302@gmail.com>
2023-04-24 23:46:58 +0200

    Add async download

--- auto_shorts/video_download/download_video.py
+++ auto_shorts/video_download/download_video.py
@@ -1,20 +1,26 @@
+import asyncio
 import json
 import os
 import shutil
-from abc import ABC, abstractmethod
 from pathlib import Path
+from typing import Protocol
 
+import numpy as np
+from loguru import logger
 from pytube import YouTube
 
 from auto_shorts.upload_to_s3 import upload_file
 from auto_shorts.video_download.download_info import (
     ChannelInfoDownloader,
+    ChannelInfoDownloaderInterface,
     VideoData,
     VideoDataWithStats,
+    VideoDataParserInterface,
+    VideoDataParser
 )
-from loguru import logger
 from auto_shorts.video_download.most_watched_moments import (
-    MostReplayedNotPresentException, MostWatchedMomentsDownloader,
+    MostReplayedNotPresentException,
+    MostWatchedMomentsDownloader,
 )
 
 base_data_path = Path(__file__).parents[2] / "data"
@@ -24,20 +30,29 @@ class VideoDataWithMoments(VideoDataWithStats):
     most_watched_moments: list[dict]
 
 
-class DownloaderBase(ABC):
-    @abstractmethod
+class DownloaderInterface(Protocol):
     def download(
-            self,
-            video_data: VideoDataWithStats,
-            save_path: Path,
-            bucket: str,
-            to_s3: bool,
-            save_local: bool,
+        self,
+        video_data: VideoData,
+        save_path: Path,
+        bucket: str,
+        to_s3: bool,
+        save_local: bool,
     ):
         """Enforce download method"""
 
+    async def download_async(
+        self,
+        video_data: VideoData,
+        save_path: Path,
+        bucket: str,
+        to_s3: bool,
+        save_local: bool,
+    ):
+        """Enforce async download method"""
 
-class YoutubeVideoDownloader(DownloaderBase):
+
+class YoutubeVideoDownloader:
     """
     A class used to download YouTube videos and save them to a specified location.
 
@@ -80,9 +95,9 @@ class YoutubeVideoDownloader(DownloaderBase):
 
     @staticmethod
     def _download_to_mp4(
-            save_path: Path,
-            vide_data_full: VideoDataWithMoments,
-            filename: str,
+        save_path: Path,
+        vide_data_full: VideoDataWithMoments,
+        filename: str,
     ) -> None:
         """
         Download the video in mp4 format and save it to a specified location.
@@ -102,20 +117,23 @@ class YoutubeVideoDownloader(DownloaderBase):
         -------
         None
         """
-        (
-            YouTube(f"https://www.youtube.com/watch?v={vide_data_full.id}")
-            .streams.filter(file_extension="mp4")
-            .first()
-            .download(str(save_path), filename=filename)
-        )
+        try:
+            (
+                YouTube(f"https://www.youtube.com/watch?v={vide_data_full.id}")
+                .streams.filter(file_extension="mp4")
+                .first()
+                .download(str(save_path), filename=filename)
+            )
+        except KeyError as e:
+            logger.error(f"Data needed to download not found. Key error: {e}")
 
     def download(
-            self,
-            video_data: VideoData,
-            save_path: Path = base_data_path,
-            bucket: str = "auto-shorts",
-            to_s3: bool = False,
-            save_local: bool = True,
+        self,
+        video_data: VideoData,
+        save_path: Path = base_data_path,
+        bucket: str = "auto-shorts",
+        to_s3: bool = False,
+        save_local: bool = True,
     ) -> None:
         """
         Download video data and save it to the specified directory.
@@ -144,7 +162,13 @@ class YoutubeVideoDownloader(DownloaderBase):
                 "Wrong params config! One of 'to_s3' and 'save_local' must be True!"
             )
 
-        video_data_full = self.download_moments(video_data=video_data)
+        try:
+            video_data_full = self.download_moments(video_data=video_data)
+
+        except MostReplayedNotPresentException as e:
+            logger.error(e)
+            return
+
         os.makedirs(save_path / video_data_full.id, exist_ok=True)
         data_save_path = save_path / video_data_full.id
         with open(data_save_path / "video_data.json", "w") as file:
@@ -153,6 +177,7 @@ class YoutubeVideoDownloader(DownloaderBase):
                 file,
                 indent=4,
             )
+
         self._download_to_mp4(
             save_path=data_save_path,
             vide_data_full=video_data_full,
@@ -177,41 +202,99 @@ class YoutubeVideoDownloader(DownloaderBase):
         if not save_local:
             shutil.rmtree(data_save_path)
 
+    async def download_async(
+        self,
+        video_data: VideoData,
+        save_path: Path = base_data_path,
+        bucket: str = "auto-shorts",
+        to_s3: bool = False,
+        save_local: bool = True,
+    ) -> None:
+        self.download(
+            video_data=video_data,
+            save_path=save_path,
+            bucket=bucket,
+            to_s3=to_s3,
+            save_local=save_local,
+        )
+
 
 class MultipleVideoDownloader:
-    def __init__(self) -> None:
-        self.downloader = YoutubeVideoDownloader()
-        self.channel_info_downloader = ChannelInfoDownloader()
+    def __init__(
+        self,
+        downloader: DownloaderInterface,
+        channel_info_downloader: ChannelInfoDownloaderInterface,
+    ) -> None:
+        self.downloader = downloader
+        self.channel_info_downloader = channel_info_downloader
+
+    def get_video_data(
+        self, video_id: str, video_number_limit: int
+    ) -> list[VideoDataWithStats]:
+        return self.channel_info_downloader.get_videos_from_channel(
+            video_id=video_id, video_number_limit=video_number_limit
+        )
 
     def download_videos_from_channel(
-            self,
-            video_id: str,
-            video_number_limit: int = 1000,
-            save_path: Path = base_data_path,
-            bucket: str = "auto-shorts",
-            to_s3: bool = False,
-            save_local: bool = True,
-    ):
-        videos_data = self.channel_info_downloader.get_videos_from_channel(
-            video_id=video_id,
-            video_number_limit=video_number_limit
+        self,
+        video_id: str,
+        video_number_limit: int = 1000,
+        save_path: Path = base_data_path,
+        bucket: str = "auto-shorts",
+        to_s3: bool = False,
+        save_local: bool = True,
+    ) -> None:
+        videos_data = self.get_video_data(
+            video_id=video_id, video_number_limit=video_number_limit
         )
         for video_data in videos_data:
-            try:
-                logger.info(f"Downloading video: {video_data.id}")
-                self.downloader.download(
-                    video_data=video_data,
-                    save_path=save_path,
-                    bucket=bucket,
-                    to_s3=to_s3,
-                    save_local=save_local,
-                )
-            except MostReplayedNotPresentException as e:
-                logger.error(e)
-                continue
+            logger.info(f"Downloading video: {video_data.id}")
+            self.downloader.download(
+                video_data=video_data,
+                save_path=save_path,
+                bucket=bucket,
+                to_s3=to_s3,
+                save_local=save_local,
+            )
+
+    async def download_videos_from_channel_async(
+        self,
+        video_id: str,
+        video_number_limit: int = 1000,
+        save_path: Path = base_data_path,
+        bucket: str = "auto-shorts",
+        to_s3: bool = False,
+        save_local: bool = True,
+        async_videos_block_size: int = 5,
+    ):
+        videos_data = self.get_video_data(
+            video_id=video_id, video_number_limit=video_number_limit
+        )
+        video_chunks = np.array_split(videos_data, async_videos_block_size)
+
+        for video_chunk in video_chunks:
+            _ = await asyncio.gather(
+                *[
+                    self.downloader.download_async(
+                        video_data=video_data,
+                        save_path=save_path,
+                        bucket=bucket,
+                        to_s3=to_s3,
+                        save_local=save_local,
+                    )
+                    for video_data in video_chunk
+                ]
+            )
 
 
 if __name__ == "__main__":
-    m_downloader = MultipleVideoDownloader()
-    m_downloader.download_videos_from_channel(video_id="1fUpkq7urDU", video_number_limit=100, to_s3=True,
-                                              save_local=True)
+    downloader_test = YoutubeVideoDownloader()
+    channel_info_downloader_test = ChannelInfoDownloader()
+    m_downloader = MultipleVideoDownloader(
+        downloader=downloader_test, channel_info_downloader=channel_info_downloader_test
+    )
+    asyncio.run(
+        m_downloader.download_videos_from_channel_async(
+            video_id="1fUpkq7urDU", video_number_limit=10, to_s3=False, save_local=True
+        )
+    )
