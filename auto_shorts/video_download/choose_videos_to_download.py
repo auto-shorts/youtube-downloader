10771a277d18aaf41a6e830787ab6df63937fea8
Jakub Wujec <kuba1302@gmail.com>
2023-04-28 00:01:17 +0200

    Isort

--- auto_shorts/video_download/choose_videos_to_download.py
+++ auto_shorts/video_download/choose_videos_to_download.py
@@ -4,7 +4,9 @@ from auto_shorts.video_download.download_info import (
     VideoDataWithStats,
     VideoInfoDownloader,
 )
-from auto_shorts.video_download.most_watched_moments import MostWatchedMomentsDownloader
+from auto_shorts.video_download.most_watched_moments import (
+    MostWatchedMomentsDownloader,
+)
 
 
 class VideoToDownloadChooseBase(ABC):
