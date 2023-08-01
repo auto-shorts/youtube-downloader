c544bd0034fdc020e0a89e08183d19d3525ccb7b
Jakub Żmujdzin <jzmujdzin@gmail.com>
2023-08-01 21:57:59 +0200

    add script to download files

--- auto_shorts/download/db/_postgres.py
+++ auto_shorts/download/db/_postgres.py
@@ -4,6 +4,9 @@ from auto_shorts.upload.db.utils import postgres_engine
 
 
 def get_video_ids_and_buckets_not_in_list(video_id_list: list[str]) -> CursorResult:
+    if not video_id_list:
+        # jak lista jest pusta to juz nie jest xdd
+        video_id_list = ['xdd']
     ids_str = ', '.join(f"'{vid_id}'" for vid_id in video_id_list)
     query = f"""
         SELECT
