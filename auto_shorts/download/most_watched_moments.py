4329b0655c31e8bbfe7e9a6663fb647af46b8a5a
Jakub Wujec <kuba1302@gmail.com>
2023-10-17 19:59:14 +0200

    use markers

--- auto_shorts/download/most_watched_moments.py
+++ auto_shorts/download/most_watched_moments.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import pprint
 
 import pandas as pd
 import requests
@@ -36,8 +37,9 @@ class MostWatchedMomentsDownloader(MostWatchedMomentsDownloaderBase):
     def _preprocess_results(raw_results: dict) -> pd.DataFrame:
         cleaned_timeframes = []
 
+        pprint.pprint(raw_results["items"][0]["mostReplayed"])
         for timeframe in raw_results["items"][0]["mostReplayed"][
-            "heatMarkers"
+            "markers"
         ]:  # check it in the future
             # print(timeframe)
             timeframe_data = timeframe["heatMarkerRenderer"]
